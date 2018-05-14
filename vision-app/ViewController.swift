3be317eece8068141075d5f7b994a7d093fb15ea
dpallan <dave@blueberrysoup.com>
2018-05-13 21:12:58 -0600

    Initial Commit

---
+++ vision-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app
+//
+//  Created by Dave Allan on 2018-05-13.
+//  Copyright © 2018 Blueberrysoup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
